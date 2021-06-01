fc7b614421e26d3a3dc630ac81783ddbc8003adb
RaviBx <ravimuay@gmail.com>
2021-06-01 17:53:48 +0530

    Added crud operation methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,59 @@ class ProductController extends Controller
     {
         $products = Product::all()->toArray();
         return array_reverse($products);
+    }
+
+
+    public function store(Request $request)
+    {
+
+        $product = new Product([
+
+                'name' => $request->input('name'),
+                'details' => $request->input('details')
+
+        ]);
+
+        $product->save();
+
+        return response()->json('Product Created !');
+        
+    }
+
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
 
 
     }
+
+
+    public function update($id, Request $request)
+    {
+
+        $product = Product::find($id);
+        $product->update($request->all());
+
+        return response()->json('Product Updated !');
+
+
+    }
+
+
+    public function destry($id)
+    {
+        
+        $product = Product::find($id);
+
+        $product->delete();
+
+        return response()->json('Product Deleted !');
+
+
+    }
+
+
+
 }
