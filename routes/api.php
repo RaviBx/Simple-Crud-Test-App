3616bbf4d676dfbebcc68f14da7cc6bfcebe5d01
RaviBx <ravimuay@gmail.com>
2021-06-02 10:11:23 +0530

    Added routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+
+Route::middleware('api')->group(function () {
+
+    Route::resource('products', ProductController::class);
+    
 });
